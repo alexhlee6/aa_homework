60a8317330c2d8ff1468f4999e5fa1881c094ef7
alex629lee <alex629lee@yahoo.com>
2020-01-14 18:54:15 -0800

    Practice tree algorithms

---
+++ W23D3/tree_order_project/lib/tree_order.js
@@ -0,0 +1,26 @@
+function inOrderArray(root) {
+    if (!root) return [];
+
+    let result = [];
+    
+    result = result.concat(inOrderArray(root.left));
+    result.push(root.val);
+    result = result.concat(inOrderArray(root.right))
+    return result;
+}
+
+function postOrderArray(root) {
+    if (!root) return [];
+    let result = [];
+
+    result = result.concat(postOrderArray(root.left));
+    result = result.concat(postOrderArray(root.right));
+    result.push(root.val);
+    return result;
+}
+
+
+module.exports = {
+    inOrderArray,
+    postOrderArray
+};
